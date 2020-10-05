d42fac0b9da2c05e2d2dbcfbfdcdb036d3185bb3
Thomas <thomtrepanier@gmail.com>
2020-10-05 14:41:50 -0400

    Exercices done

--- exercice.py
+++ exercice.py
@@ -3,16 +3,84 @@
 
 
 def check_brackets(text, brackets):
-	return False
+	#TODO: Associer fermantes à ouvrante
+	dictBrackets = {brackets[i] : brackets[i+1] for i in range(0, len(brackets), 2)}
+
+	bracketStack = []
+	i = 0
+	bracketStack.append(text[i])
+
+	while(len(bracketStack) > 0):
+		i += 1
+		if(i >= len(text)): return False
+
+		if(text[i] in dictBrackets):
+			bracketStack.append(text[i])
+		elif (text[i] in dictBrackets.values()):
+			lastBracket = bracketStack.pop()
+			if(dictBrackets[lastBracket] != text[i]): return False
+
+	return len(bracketStack) == 0
 
 def remove_comments(full_text, comment_start, comment_end):
+	while True:
+		nextCommentStartId = full_text.find(comment_start)
+		nextCommentEndId = full_text.find(comment_end)
+
+		if(nextCommentStartId == -1 and nextCommentEndId == -1):
+			return full_text
+
+		if(nextCommentStartId >= nextCommentEndId or (nextCommentStartId == -1) != (nextCommentEndId == -1)):
+			return None
+
+		full_text = full_text[:nextCommentStartId] + full_text[nextCommentEndId + len(comment_end):]
+
 	return ""
 
 def get_tag_prefix(text, opening_tags, closing_tags):
+	if text[0] != opening_tags[0][0]:
+		return (None, None)
+
+	firstEndBalise = text.find(">")
+	balise = text[:firstEndBalise + 1]
+
+	if balise in opening_tags:
+		return (balise, None)
+	elif balise in closing_tags:
+		return (None, balise)
+
 	return (None, None)
 
 def check_tags(full_text, tag_names, comment_tags):
-	return False
+
+	#1 Enlever comments
+	noCommentsText = remove_comments(full_text, comment_tags[0], comment_tags[1])
+	if(noCommentsText is None):
+		return False
+
+	#2 Créer balises
+	balisesOuvrantes = ["<" + tag_names[i] + ">" for i in range(0, len(tag_names))]
+	balisesFermantes = ["</" + tag_names[i] + ">" for i in range(0, len(tag_names))]
+	#3 ...
+	baliseStack = []
+
+	while len(noCommentsText) > 0:
+		nextBalise = get_tag_prefix(noCommentsText, tuple(balisesOuvrantes), tuple(balisesFermantes))
+
+		if(nextBalise == (None, None)): #Si on n'a pas de balise en début de ligne
+			noCommentsText = noCommentsText[1:] # On avance d'un caractère
+		elif nextBalise[0] is not None: #Si on a une balise ouvrante en début de ligne
+			baliseStack.append(nextBalise)
+			noCommentsText = noCommentsText[len(nextBalise[0]):]
+
+		else: #Si on a une balise fermante en début de ligne
+			lastBalise = baliseStack.pop() #On prend la dernière balise ouvrante
+			if len(baliseStack) == 0 or balisesOuvrantes.index(lastBalise[0]) == balisesFermantes.index(nextBalise[1]): #Si la balise fermante correspond à la dernière balise ouvrante
+				noCommentsText = noCommentsText[len(nextBalise[1]):]
+			else:
+				return False
+
+	return len(baliseStack) == 0
 
 
 if __name__ == "__main__":
